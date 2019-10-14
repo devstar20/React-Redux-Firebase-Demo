113a9ec32a0510f408d1a87c08034c56886bfdf0
devstart20 <ninjadev2020@gmail.com>
2019-10-14 14:24:21 -0700

    Initial commit

---
+++ marioplan/src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        werwe
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
