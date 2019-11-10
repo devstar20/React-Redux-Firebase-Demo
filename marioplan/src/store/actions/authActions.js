6f8e3c497f433fbf4e64e72a2c04e847229d5e95
devstart20 <ninjadev2020@gmail.com>
2019-11-10 22:10:30 +0800

    Add authAction.js anthentication functionality

--- marioplan/src/store/actions/authActions.js
+++ marioplan/src/store/actions/authActions.js
@@ -3,13 +3,13 @@ export const signIn = (credentials) => {
     return(dispatch, getState, { getFirebase, getFirestore }) => {
        
         const firebase = getFirebase()
-        // firebase.auth().signInWithEmailAndPassword(
-        //     credentials.email,
-        //     credentials.password
-        // ).then(() => {
-        //     dispatch({type: 'LOGIN_SUCCESS'})
-        // }).catch((err) => {
-        //     dispatch({type: 'LOGIN_ERROR', err})
-        // })
+        firebase.auth().signInWithEmailAndPassword(
+            credentials.email,
+            credentials.password
+        ).then(() => {
+            dispatch({type: 'LOGIN_SUCCESS'})
+        }).catch((err) => {
+            dispatch({type: 'LOGIN_ERROR', err})
+        })
     }
 }
