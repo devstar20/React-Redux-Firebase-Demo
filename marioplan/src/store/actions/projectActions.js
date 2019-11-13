8b6f9e10311134071083589dfd36adc442b09737
devstart20 <ninjadev2020@gmail.com>
2019-11-14 00:03:50 +0800

    Update the project Actions
    update the hard code of project author details

--- marioplan/src/store/actions/projectActions.js
+++ marioplan/src/store/actions/projectActions.js
@@ -2,17 +2,20 @@ export const createProject = (project) =>{
     return (dispatch, getState, { getFirebase, getFirestore }) => {
         //make async call to database
         const firestore = getFirestore();
+        const profile = getState().firebase.profile
+        console.log(profile)
+        const userId = getState().firebase.auth.uid
+        console.log(userId)
         firestore.collection('projects').add({
             ...project,
-            authorFirstName: 'Net',
-            authorLastName: 'Ninja',
-            authorId: 12345,
+            authorFirstName: profile.firstName,
+            authorLastName: profile.lastName,
+            authorId: userId,
             createAt: new Date()
         }).then(() =>{
             dispatch({type: 'CREATE_PROJECT', project})
         }).catch((err) =>{
             dispatch({type:'CREATE_PROJECT_ERROR', err});
         })
-        
     }
 }
